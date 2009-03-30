75a866e3c499b2720e87d2142541a77089748b60
Mislav Marohnić <mislav.marohnic@gmail.com>
2009-03-30 15:02:52 +0800

    cleanup specs
    Signed-off-by: Carlos Paramio <carlos@evolve.st>

--- spec/facebook_spec.rb
+++ spec/facebook_spec.rb
@@ -27,23 +27,25 @@ describe Rack::Facebook do
     sign_hash(hash, API_KEY)
   end
   
-  def post_env(params)
-    env = {"rack.request.form_hash" => params}
-    env["rack.request.form_input"] = env["rack.input"] = "fb"
-    env
-  end
-  
-  def post(app, params)
-    mock_post app, post_env(params)
-  end
-  
   def mock_post(app, env)
     facebook = described_class.new(app, :application_secret => SECRET, :api_key => API_KEY)
     request = Rack::MockRequest.new(facebook)
     @response = request.post("/", env)
   end
   
+  def post_env(params)
+    # set up form variables like rack::request had parsed them
+    env = {"rack.request.form_hash" => params}
+    env["rack.request.form_input"] = env["rack.input"] = "fb"
+    env
+  end
+  
+  def post_request(app, params)
+    mock_post app, post_env(params)
+  end
+  
   def cookie_env(cookies)
+    # set up the cookie hash like rack::request would
     env = {"rack.request.cookie_hash" => cookies}
     env["rack.request.cookie_string"] = env["HTTP_COOKIE"] = "fb"
     env
@@ -51,6 +53,10 @@ describe Rack::Facebook do
   
   def cookie_request(app, cookies)
     mock_post app, cookie_env(cookies)
+  end
+  
+  def request
+    @request
   end
   
   def response
@@ -61,69 +67,67 @@ describe Rack::Facebook do
     [status, {"Content-type" => "test/plain", "Content-length" => "5"}, ["hello"]]
   end
   
-  describe 'without a block' do
-    describe 'when the fb_sig is not valid' do
-      it 'should return 400 Invalid Facebook signature' do
-        post mock('rack app'), "fb_sig" => "INVALID"
+  def app
+    @app ||= lambda do |env|
+      @request = Rack::Request.new(env)
+      response_env
+    end
+  end
+  
+  describe "without a block" do
+    describe "when the signature is not valid" do
+      it "should fail with 400 Invalid Facebook signature" do
+        post_request mock('rack app'), "fb_sig" => "INVALID"
         response.status.should == 400
       end
     end
     
-    describe 'when the fb_sig is valid' do
-      it 'should convert the facebook parameters to ruby objects' do
-        app = lambda do |env|
-          @env = env
-          @params = Rack::Request.new(env).params
-          response_env
-        end
-        
-        post app, sign_params("fb_sig_in_canvas" => "1", "fb_sig_time" => "1", "fb_sig_user" => "234433")
-        response.status.should == 200
-        
-        @params['fb_sig'].should be_nil
-        @params['fb_sig_time'].should be_nil
-        @params['fb_sig_user'].should be_nil
-        @params['fb_sig_in_canvas'].should be_nil
-        
-        @env['facebook.time'].should == Time.at(1)
-        @env['facebook.in_canvas'].should be_true
-        @env['facebook.user'].should == "234433"
+    describe "when the fb_sig is valid" do
+      def post(params)
+        post_request(app, sign_params(params))
       end
       
-      it 'should not touch parameters not prefixed with "fb_sig"' do
-        app = lambda do |env|
-          params = Rack::Request.new(env).params
-          params['foo'].should == 'bar'
-          response_env
-        end
+      it "should convert the facebook parameters to ruby objects" do
+        post "fb_sig_in_canvas" => "1", "fb_sig_time" => "1", "fb_sig_user" => "234433"
         
-        post app, sign_params("fb_sig_user" => "234433", "foo" => "bar")
-        response.status.should == 200
+        request.env['facebook.time'].should == Time.at(1)
+        request.env['facebook.in_canvas'].should be_true
+        request.env['facebook.user'].should == "234433"
+      end
+      
+      it "should strip facebook parameters from params hash" do
+        post "fb_sig_in_canvas" => "1", "fb_sig_user" => "234433"
+        
+        request['fb_sig'].should be_nil
+        request['fb_sig_user'].should be_nil
+        request['fb_sig_in_canvas'].should be_nil
+      end
+      
+      it "should not touch parameters not prefixed with \"fb_sig\"" do
+        post "fb_sig_user" => "234433", "foo" => "bar"
+        
+        request['foo'].should == 'bar'
       end
       
       it "should split friend IDs into an array" do
-        app = lambda do |env|
-          env["facebook.friends"].should == ["2", "3", "5"]
-          response_env
-        end
-        post app, sign_params("fb_sig_friends" => "2,3,5")
-        response.status.should == 200
+        post "fb_sig_friends" => "2,3,5"
+        
+        request.env["facebook.friends"].should == ["2", "3", "5"]
       end
       
-      it 'should convert the request method from POST to the original client method' do
-        app = mock('rack app')
-        app.should_receive(:call).with { |env|
-          env['REQUEST_METHOD'].should == 'PUT'
-          env['facebook.original_method'].should == 'POST'
-          true
-        }.and_return(response_env)
-        post app, sign_params("fb_sig_request_method" => "PUT")
+      it "should convert the request method from POST to the original client method" do
+        post "fb_sig_request_method" => "PUT"
+        
+        request.request_method.should == 'PUT'
+        request.env['facebook.original_method'].should == 'POST'
       end
 
-      it 'should run app' do
-        app = mock('rack app')
-        app.should_receive(:call).with(instance_of(Hash)).and_return(response_env)
-        post app, sign_params("fb_sig_foo" => "bar")
+      it "should call app" do
+        @app = mock('rack app')
+        @app.should_receive(:call).with(instance_of(Hash)).and_return(response_env)
+        
+        post "fb_sig_foo" => "bar"
+        response.status.should == 200
       end
     end
     
@@ -131,6 +135,7 @@ describe Rack::Facebook do
       it "should run app if cookie signature is valid" do
         app = mock('rack app')
         app.should_receive(:call).with(instance_of(Hash)).and_return(response_env)
+        
         cookie_request app, sign_cookies("#{API_KEY}_user" => "22", "#{API_KEY}_ss" => "SEKRIT")
         response.status.should == 200
       end
